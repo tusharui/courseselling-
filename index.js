c79717192a3066ea6e038c95f2c9d541435805d4
Tushar <tusharanshu18@gmail.com>
2025-08-06 10:53:46 +0530

    start if database is up

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const jsonwebtoken =require("jsonwebtoken");
 const { courseRouter } = require("./routes/course");
 const {adminRouter} = require("./routes/admin");
 const JWT_SECRET ="jhgvcfghkjh"
+const mongoose = require("mongoose");
 
 app.use("/api/v1/user",useRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin",adminRouter);
 
+async function main(){
+    await mongoose.connect("")
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
 
-
-app.listen(3000);
+main()
