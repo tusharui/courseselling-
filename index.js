209b14071208100b1d4eedaee19699b82d4473a5
Tushar <tusharanshu18@gmail.com>
2025-08-06 07:16:35 +0530

    improce code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const jsonwebtoken =("jsonwebtoken");
+const jsonwebtoken =require("jsonwebtoken");
 const JWT_SECRET ="jhgvcfghkjh"
 
 app.post('/user/signup',function(req,res){
@@ -10,23 +10,29 @@ app.post('/user/signup',function(req,res){
 })
 
 
-app.post('/user/signup',function(req,res){
-    res.json({
-        message:"signin endpoint "
-    })
+app.post('/user/signin ',function(req,res){
+   res.json({
+        message: "signin endpoint"
+    });
 })
 
 app.get('/user/purchases',function(req,res){
-    
+     res.json({
+        message: "user purchases"
+    });
 })
 
 
 app.get('/courses/purchase',function(req,res){
-
+    res.json({
+        message: "purchase a course"
+    });
 })
 
-app.get('/coureses',function(req,res){
-
+app.get('/courses',function(req,res){
+    res.json({
+        message: "all courses"
+    });
 })
 
 
